623e72c73c3bff28c6c520e689a32a8ba410e193
KateChubarava <k.chubarava@altoros.com>
2023-06-13 21:49:45 +0200

    finish model

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RepeatedKFold
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasRegressor
+from keras.layers import Dense, LeakyReLU
+import numpy as np
 
 # check data for nulls, duplicates
 # divide set to X and Y
@@ -64,60 +64,64 @@ def grid_search_cv_gradient_boost():
     print(grid.best_params_)
 
 
-def model_building_function(X, n_outputs_, hidden_layer_sizes):
-    model = Sequential()
-    model.add(Dense(X.shape[1], activation="relu", input_shape=X.shape[1:]))
-    for size in hidden_layer_sizes:
-        model.add(Dense(size, activation="relu"))
-    model.add(Dense(n_outputs_, activation="relu"))
-    model.compile("adam", loss="mean_absolute_error")
-    return model
+# def model_building_function(X, n_outputs_, hidden_layer_sizes):
+#     model = Sequential()
+#     model.add(Dense(X.shape[1], activation="relu", input_shape=X.shape[1:]))
+#     for size in hidden_layer_sizes:
+#         model.add(Dense(size, activation="relu"))
+#     model.add(Dense(n_outputs_, activation="relu"))
+#     model.compile("adam", loss="mean_absolute_error")
+#     return model
 
 
-def keros_regressor():
-    estimator = KerasRegressor(build_fn=model_building_function(X, 1, hidden_layer_sizes=[20, 10, 5]))
-    estimator.fit(X_train, y_train)
-
-    print(estimator.score(X_test, y_test))
-    print(estimator.score(X_train, y_train))
-
-    y_prediction = estimator.predict(X_test)
-    y_train_prediction = estimator.predict(X_train)
-
-    print(mae(y_test, y_prediction))
-    print(mae(y_train, y_train_prediction))
-
-    y_submission = estimator.predict(X_submission.drop('id', axis='columns'))
-    write_to_file(y_submission, X_submission)
+# def keros_regressor():
+#     estimator = KerasRegressor(build_fn=model_building_function(X, 1, hidden_layer_sizes=[20, 10, 5]))
+#     estimator.fit(X_train, y_train)
+#
+#     print(estimator.score(X_test, y_test))
+#     print(estimator.score(X_train, y_train))
+#
+#     y_prediction = estimator.predict(X_test)
+#     y_train_prediction = estimator.predict(X_train)
+#
+#     print(mae(y_test, y_prediction))
+#     print(mae(y_train, y_train_prediction))
+#
+#     y_submission = estimator.predict(X_submission.drop('id', axis='columns'))
+#     write_to_file(y_submission, X_submission)
 
 
 def get_model(X_train_normalized):
     model = Sequential()
     model.add(Dense(128, input_shape=(X_train_normalized.shape[1],)))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(1, activation='linear'))
+    model.add(Dense(64, activation=LeakyReLU(alpha=0.1)))
+    model.add(Dense(64, activation=LeakyReLU(alpha=0.1)))
+    model.add(Dense(1, activation=LeakyReLU(alpha=0.1)))
     model.compile(optimizer='adam', loss='mean_absolute_error')
     return model
 
 
 def chat_gpt_keras_test():
     # X_train_normalized = (X_train - np.mean(X_train, axis=0)) / np.std(X_train, axis=0)
-
     model = get_model(X_train)
-    model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.2)
+    model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.33)
 
     # X_test_normalized = (X_test - np.mean(X_test, axis=0)) / np.std(X_test, axis=0)
 
     y_prediction = model.predict(X_test)
-
+    # output = pd.Series({'id': X_test_normalized.id, 'Age': y_prediction.reshape(len(y_prediction))})
+    y_prediction = np.round(y_prediction)
     print(mae(y_prediction, y_test))
 
-    # _X_submission = X_submission.drop('id', axis='columns')
+    _X_submission = X_submission.drop('id', axis='columns')
     # X_submission_normalized = (_X_submission - np.mean(_X_submission, axis=0)) / np.std(_X_submission, axis=0)
-    # y_submission = model.predict(X_submission_normalized)
+    y_submission = model.predict(_X_submission)
+    #
+    write_to_file(np.round(y_submission), X_submission)
 
-    # write_to_file(y_submission, X_submission)
 
 chat_gpt_keras_test()  # 1.3891355250165365, 1.3824723439584041 - remove outliners
 
+# с нормализацией
+# без нормализации
+# без нормолизации и без удаления выбросов
--- models.py
+++ models.py
@@ -5,14 +5,9 @@ from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RepeatedKFold
 from sklearn.metrics import mean_squared_error as mse
-from keras.layers import LeakyReLU, Dropout, Lambda
-from keras.layers import Dense, Activation
-from keras.wrappers.scikit_learn import KerasRegressor
-from keras import backend as K
-from keras.models import Sequential
 
 
-def grid_search_cv_random_forest(X,Y):
+def grid_search_cv_random_forest(X, Y):
     cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=100)
     param_grid = {'n_estimators': [100, 200],
                   'min_samples_split': [8, 10], 'min_samples_leaf': [3, 4, 5],
@@ -33,6 +28,7 @@ def svc(X_train, y_train, X_test, y_test):
     predictions = model.predict(X_test)
     print(mse(y_test, predictions))
 
+
 def kneighbors_classifier(X_train, y_train, X_test, y_test):
     model = KNeighborsClassifier(n_neighbors=20)
     model.fit(X_train, y_train)
@@ -41,6 +37,7 @@ def kneighbors_classifier(X_train, y_train, X_test, y_test):
 
     print('kneighbors classifier')
     print(mse(y_test, predictions))
+
 
 def kneighbors_regressor(X_train, y_train, X_test, y_test):
     model = KNeighborsRegressor(n_neighbors=15)
@@ -68,36 +65,3 @@ def tree_classifier(X_train, y_train, X_test, y_test):
 
     print('tree_classifier')
     print(mse(y_test, predictions))
-
-
-def create_model(optimizer='adam', activation=Activation('relu'), dropout_rate=0.0, log_normalization=False):
-    model = Sequential()
-
-    if log_normalization:
-        model.add(Lambda(lambda x: K.log(x + 1e-8)))  # Apply log transformation
-
-    model.add(Dense(128, activation=activation, input_shape=(X.shape[1],)))
-    model.add(Dropout(dropout_rate))
-    model.add(Dense(64, activation=activation))
-    model.add(Dropout(dropout_rate))
-    model.add(Dense(32, activation=activation))
-    model.add(Dropout(dropout_rate))
-    model.add(Dense(1, activation='linear'))
-    model.compile(optimizer=optimizer, loss='mean_absolute_error')
-    return model
-
-
-def chat_gpt_keras_regressor_grid_search(X_train, y_train, X_test, y_test):
-    keras_regressor = KerasRegressor(build_fn=create_model, verbose=0)
-    param_grid = {
-        'optimizer': ['adam', 'sgd'],
-        'activation': [LeakyReLU(alpha=0.1), Activation('relu')],
-        'dropout_rate': [0.0, 0.2, 0.4],
-        'log_normalization': [False, True]
-    }
-
-    grid_search = GridSearchCV(estimator=keras_regressor, param_grid=param_grid, cv=5, verbose=3)
-    grid_search.fit(X_train, y_train)
-
-    print("Best parameters: ", grid_search.best_params_)
-    print("Best Score: ", grid_search.best_score_)
